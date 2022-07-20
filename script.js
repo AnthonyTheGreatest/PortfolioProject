c3292977cece235e9bf197f7bba4d143499d23ff
Miklós Milosevits <milosevitsmiklos@gmail.com>
2022-07-20 14:24:20 +0200

    Sort of finish project.

---
+++ script.js
@@ -0,0 +1,6 @@
+// Change the style of the photo of Ada Lovelace when someone clicks on it.
+
+const ada = document.querySelector("#ada");
+ada.onclick = function() {
+    ada.style.borderRadius = "0%"
+}
